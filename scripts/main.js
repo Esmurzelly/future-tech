c852370f26e8d5204e4a02cd0b2d6cf9f7d958c8
Dark Lord <adamsuper148@gmail.com>
2025-05-01 14:07:45 +0300

    number validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 import Header from "./Header.js";
 import TabsCollection from './Tabs.js'
 import VideoPlayerCollection from "./VideoPlayer.js";
-import ExpandableContentCollection from "./ExpandableContent.js"; "./ExpandableContent.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
+import InputMaskCollection from "./inputMask.js";
 
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
 new ExpandableContentCollection();
+new InputMaskCollection();
